942b11c8e424d19de14ff4e77a42320a5fc14df1
dievee <nex7440@gmail.com>
2017-09-09 21:53:17 +0300

    add ability to start/finish participants add ui

--- RunFest/Controllers/RunController.cs
+++ RunFest/Controllers/RunController.cs
@@ -18,11 +18,39 @@ namespace RunFest.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Start()
+        public IActionResult Start()
         {
             List<User> Users = _userManager.Users.Where( User => User.StartTime.CompareTo(emptyTime) == 0 ).ToList();
 
             return View(Users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Start(string userId)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(userId);
+                if (user != null)
+                {
+                    user.StartTime = DateTimeOffset.Now;
+
+                    var result = await _userManager.UpdateAsync(user);
+                    if (!result.Succeeded)
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+                List<User> Users = _userManager.Users.Where(User => User.StartTime.CompareTo(emptyTime) == 0).ToList();
+                return View(Users);
+            }
+            else
+            {
+                return View();
+            }            
         }
 
         [HttpGet]
@@ -33,6 +61,35 @@ namespace RunFest.Controllers
             return View(Users);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Finish(string userId)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(userId);
+                if (user != null)
+                {
+                    user.FinishTime = DateTimeOffset.Now;
+
+                    var result = await _userManager.UpdateAsync(user);
+                    if (!result.Succeeded)
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+                List<User> Users = _userManager.Users.Where(User => User.StartTime.CompareTo(emptyTime) > 0 &&
+                                                                User.FinishTime.CompareTo(emptyTime) == 0).ToList();
+                return View(Users);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
         [HttpGet]
         public IActionResult Finished()
         {
@@ -40,5 +97,7 @@ namespace RunFest.Controllers
                                                                 User.FinishTime.CompareTo(emptyTime) > 0).ToList();
             return View(Users);
         }
+
+
     }
 }
